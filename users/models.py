79e6248fa164e20a244dd8185c93ed1893c7f1ef
jonathanmir <jonathanmir8@gmail.com>
2023-05-03 11:52:23 -0300

    feat/AddBaseStructure

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Utilizar a classe AbstractUser do django e modificar de acordo com a model que criamos
+
+# class User(AbstractUser):
+#     ...
