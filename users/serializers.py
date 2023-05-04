dea0908a2088398737d118b8dabf8d5dc9fb80b5
Henrique Longhi Junior <juniorlonghi13@gmail.com>
2023-05-04 16:06:05 -0300

    fix: serializer updated

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    def create(self, validated_data: dict) -> User:
-        return User.objects.create_user(**validated_data)
+    name = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with that name already exists.",
+            )
+        ],
+    )
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+
+    # def create(self, validated_data: dict) -> User:
+    #     return User.objects.create_superuser(**validated_data)
+
+    # def update(self, instance: User, validated_data: dict) -> User:
+    #     for key, value in validated_data.items():
+    #         if key == "password":
+    #             instance.set_password(value)
+    #         else:
+    #             setattr(instance, key, value)
+
+    #     instance.save()
+
+    #     return instance
 
     class Meta:
         model = User
@@ -16,5 +40,5 @@ class UserSerializer(serializers.ModelSerializer):
             "password",
             "is_superuser",
         ]
-
+        read_only_fields = ["is_superuser"]
         extra_kwargs = {"password": {"write_only": True}}
