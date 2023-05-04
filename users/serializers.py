a860e9d8991b9ab17acd0dcfb0e5cc515bf8c94d
Henrique Longhi Junior <juniorlonghi13@gmail.com>
2023-05-04 15:33:40 -0300

    feat: user model and serializer

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+
+class AccountSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_user(**validated_data)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "name",
+            "email",
+            "birthdate",
+            "password",
+            "is_superuser",
+        ]
+
+        extra_kwargs = {"password": {"write_only": True}}
