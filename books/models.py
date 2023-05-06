cdb7c9cc2f9eaa3e1adab815af560e6051d6de6d
isac <jeandersantos0310@gmail.com>
2023-05-06 12:50:50 -0300

    feat/viewsBook

--- books/models.py
+++ books/models.py
@@ -2,18 +2,16 @@ from django.db import models
 from users.serializers import User
 
 
-# Model do Livro que também deve ter um campo read only com uma lógica que leia quantas Copy pertencem ao ID do próprio Book.
 class Book(models.Model):
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
 
 
-# Model do Seguindo, contém a ForeignKey do usuário e ForeignKey de Book
 class Follow(models.Model):
     book = models.ForeignKey(
-        Book, on_delete=models.CASCADE, related_name="follows", null=True
+        Book, on_delete=models.CASCADE, related_name="book", null=True
     )
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="follows", null=True
+        User, on_delete=models.CASCADE, related_name="user", null=True
     )
--- books/serializers.py
+++ books/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 
 
 class BookSerializer(serializers.ModelSerializer):
+    def create(self, validated_data: dict) -> Book:
+        return Book.objects.create(**validated_data)
+
     class Meta:
         model = Book
-        fields = ["id", "name", "author", "category", "copy", "follow"]
+        fields = ["id", "name", "author", "category"]
